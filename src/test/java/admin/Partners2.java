dab547923c65a06ac12d6715588a4b19177602a7
cmlim <suitos@naver.com>
2020-10-27 13:23:34 +0900

    파트너 지사변경 케이스 추가 파트너사 변경 케이스(partnerSelect_valid2) 문제 있음 수정 필요

--- src/test/java/admin/Partners2.java
+++ src/test/java/admin/Partners2.java
@@ -24,6 +24,7 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.ITestContext;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -37,6 +38,8 @@ import org.testng.annotations.Test;
  * 14. 파트너사 변경  케이스 1, C를 B밑으로 시도
  * 15. 파트너사 변경  케이스 2, C를 한국지사 밑으로 시도
  * 20. 파트너 삭제 시도. 고객사가 있는 파트너(TestE)
+ * 
+ * 30. 파트너사 변경  케이스 3, 지사변경
  */
 
 public class Partners2 {
@@ -65,6 +68,7 @@ public class Partners2 {
 	public static String PARTNER_TESTEUSER = "TestEUser";
 	
 	public static String PARTNER_KR_NAME = "한국지사";
+	public static String PARTNER_JP_NAME = "일본지사";
 	
 	public static String findItem = ".//span[@title='%s']";
 	public static WebDriver driver;
@@ -456,10 +460,12 @@ public class Partners2 {
 			String item = String.format(findItem, PARTNER_KR_NAME);
 			for(int i = 0 ; i < 10 ; i++) {
 				if (isElementPresent(tree, By.xpath(item))) {
+					System.out.println("$$$find korea partner");
 					break;	
 				} else {
 					List<WebElement> pas = driver.findElements(By.xpath(XPATH_PARTNER_SELECTBOX_ITEM));
 					js.executeScript("arguments[0].scrollIntoView(true);", pas.get(0));
+		
 					Thread.sleep(100);
 				}
 			}
@@ -668,7 +674,101 @@ public class Partners2 {
 			Exception e = new Exception(failMsg);
 			throw e;
 		}
-	}	
+	}
+	
+	// 30. 파트너사 변경  케이스 3, 지사변경
+	@Test(priority = 30, enabled = true)
+	public void partnerSelect_invalid3() throws Exception {
+		String failMsg = "";
+		
+		CommonValues comm = new CommonValues();
+		comm.logoutaddmin(driver);
+		
+		comm.logintadmin(driver, CommonValues.USER_SYSADMIN);
+		
+		checkListView(driver);
+		
+		driver.findElement(By.xpath(CommonValues.XPATH_LIST_SEARCH_INPUTBOX)).sendKeys(PARTNER_TESTA);
+		driver.findElement(By.xpath(CommonValues.XPATH_LIST_SEARCH_BTN)).click();
+		Thread.sleep(500);
+		
+		List<WebElement> rows = driver.findElements(By.xpath(CommonValues.XPATH_LIST_ROW_ITEM));
+		
+		if(rows.size() != 1) {
+			failMsg = "1. searched item error [Expected]1rows [Actual]" + rows.size();
+		} else {
+			rows.get(0).click();
+			Thread.sleep(500);
+			
+			PartnerID = driver.getCurrentUrl().replace(CommonValues.ADMIN_URL + CommonValues.URL_PARTNEREDIT, "");
+			
+			driver.findElement(By.xpath(XPATH_PARTNER_SELECTBOX)).click();
+			Thread.sleep(500);
+			WebElement tree = driver.findElement(By.xpath(XPATH_PARTNER_SELECTTREE));
+
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			String item = String.format(findItem, PARTNER_JP_NAME);
+			for(int i = 0 ; i < 10 ; i++) {
+				if (isElementPresent(tree, By.xpath(item))) {
+					js.executeScript("arguments[0].scrollIntoView(true);", tree.findElement(By.xpath(item)));
+					Thread.sleep(100);
+					break;	
+				} else {
+					List<WebElement> pas = driver.findElements(By.xpath(XPATH_PARTNER_SELECTBOX_ITEM));
+					js.executeScript("arguments[0].scrollIntoView(true);", pas.get(pas.size()-1));
+					Thread.sleep(100);
+				}
+			}
+			tree.findElement(By.xpath(item)).click();
+			Thread.sleep(500);
+			
+			if(!driver.findElement(By.xpath(Partners.XPATH_PARTNER_PARTNERS)).getText().contentEquals(PARTNER_JP_NAME)) {
+				failMsg = failMsg + "\n2. selected partner. [Expected]" + PARTNER_JP_NAME 
+						+ " [Actual]" + driver.findElement(By.xpath(Partners.XPATH_PARTNER_PARTNERS)).getText();
+			}
+			
+			driver.findElement(By.xpath(Partners.XPATH_PARTNER_SUBMIT_BTN)).click();
+			Thread.sleep(500);
+			
+			if(!isElementPresent(By.xpath(Partners.XPATH_PARTNER_ERROR_TOAST))) {
+				failMsg = failMsg + "\n3. cannot find toast.";
+			} else {
+				if(!driver.findElement(By.xpath(Partners.XPATH_PARTNER_ERROR_TOAST)).getText().contentEquals(MSG_PARTNER_INVALID_PARTNER)) {
+					failMsg = failMsg + "\n4. toast message. [Expected]" + MSG_PARTNER_INVALID_PARTNER 
+							+ " [Actual]" + driver.findElement(By.xpath(Partners.XPATH_PARTNER_ERROR_TOAST)).getText();
+				}
+			}
+			
+			driver.findElement(By.xpath(Partners.XPATH_PARTNER_LIST_BTN)).click();
+			Thread.sleep(100);
+			
+		}
+		
+		checkListView(driver);
+		
+		driver.findElement(By.xpath(CommonValues.XPATH_LIST_SEARCH_INPUTBOX)).sendKeys(PARTNER_TESTA);
+		driver.findElement(By.xpath(CommonValues.XPATH_LIST_SEARCH_BTN)).click();
+		Thread.sleep(500);
+		
+		rows = driver.findElements(By.xpath(CommonValues.XPATH_LIST_ROW_ITEM));
+		
+		if(rows.size() != 1) {
+			failMsg = failMsg + "\n5. searched item error [Expected]1rows [Actual]" + rows.size();
+		} else {
+			rows.get(0).click();
+			Thread.sleep(500);
+			
+			if(!driver.findElement(By.xpath(Partners.XPATH_PARTNER_PARTNERS)).getText().contentEquals(PARTNER_KR_NAME)) {
+				failMsg = failMsg + "\n6. selected partner. [Expected]" + PARTNER_KR_NAME 
+						+ " [Actual]" + driver.findElement(By.xpath(Partners.XPATH_PARTNER_PARTNERS)).getText();
+			}
+		}
+		
+		if (failMsg != null && !failMsg.isEmpty()) {
+			Exception e = new Exception(failMsg);
+			throw e;
+		}
+	}
 
 	private void checkListView(WebDriver wd) throws InterruptedException {
 		if(!wd.getCurrentUrl().contentEquals(CommonValues.ADMIN_URL + CommonValues.URL_PARTNERLIST)) {
